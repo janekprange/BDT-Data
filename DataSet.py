07c23bc369b5636f45e9f85f110777d8396d63ca
Janek Prange <Janek.Prange@posteo.de>
2023-11-12 17:37:57 +0100

    formatting and annotations

--- DataSet.py
+++ DataSet.py
@@ -1,27 +1,30 @@
 import pandas as pd
 import numpy as np
+from typing import Tuple
+
 
 def serialize_row(row: pd.Series) -> str:
-  result = ""
-  for index, value in row.items():
-    result += f"{index}: {value}, "
-  return result[:-2] + "?"
+    result = ""
+    for index, value in row.items():
+        result += f"{index}: {value}, "
+    return result[:-2] + "?"
 
-'''This class encasulates all the functionality related to the datasets.'''
-class DataSet():
- 
+
+class DataSet:
+    """This class encasulates all the functionality related to the datasets."""
+
     def __init__(self, path_dirty: str, path_clean: str):
         self.dirty_set: pd.DataFrame = pd.read_csv(path_dirty)
         self.clean_set: pd.DataFrame = pd.read_csv(path_clean)
 
-    '''Returns a dataset as an Dataframes based on the calling object. 
-    The "dirty" argument decides whether the dirty dataset or the clean dataset will be returned.'''
     def get(self, dirty: bool) -> pd.DataFrame:
+        """Returns a dataset as an Dataframes based on the calling object.
+        The "dirty" argument decides whether the dirty dataset or the clean dataset will be returned.
+        """
         return self.dirty_set if dirty else self.clean_set
-    
-    '''Returns two DataFrames with "amount" random sample rows. There are no duplicate rows.'''
-    def random_sample(self, amount: int) -> (pd.DataFrame, pd.DataFrame):
 
+    def random_sample(self, amount: int) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """Returns two DataFrames with "amount" random sample rows. There are no duplicate rows."""
         # Generate a random permutation of row indices
         rng = np.random.default_rng()  # Create a random number generator
         permutation = rng.permutation(len(self.dirty_set))
@@ -30,37 +33,39 @@ class DataSet():
         dirty_shuffled = self.dirty_set.iloc[permutation[:amount]]
         clean_shuffled = self.clean_set.iloc[permutation[:amount]]
 
-        return (dirty_shuffled, clean_shuffled) 
-    
-    '''Returns a string with "amount" corrected random sample rows. There are no duplicate rows.'''
-    def generate_examples(self, column_id: int, amount: int = 1) -> str:
+        return (dirty_shuffled, clean_shuffled)
 
+    def generate_examples(self, column_id: int, amount: int = 1) -> str:
+        """Returns a string with `amount` corrected random sample rows. There are no duplicate rows."""
         # this call already generates the sample
         sampleDataDirty, sampleDataClean = self.random_sample(amount)
-        
+
         # it is now a matter of converting the sample into a string
         result_str = ""
         for i in range(amount):
             result_str += serialize_row(sampleDataDirty.iloc[i])
-            
+
             # the example string also contains corrections of the sample rows
-            error_string = " No" 
-            if(sampleDataDirty.iloc[i, column_id] != sampleDataClean.iloc[i, column_id]):
+            error_string = " No"
+            if sampleDataDirty.iloc[i, column_id] != sampleDataClean.iloc[i, column_id]:
                 error_string = " Yes"
             result_str += error_string + "\n"
-            
+
         return result_str[:-1]
-    
+
 
 class Flights(DataSet):
     def __init__(self):
         super().__init__("./prepared/flights_dirty.csv", "./prepared/flights_clean.csv")
-    
+
+
 class Food(DataSet):
     def __init__(self):
         super().__init__("./prepared/food_dirty.csv", "./prepared/food_clean.csv")
 
+
 class Hospital(DataSet):
     def __init__(self):
-        super().__init__("./prepared/hospital_dirty.csv", "./prepared/hospital_clean.csv")
-    
+        super().__init__(
+            "./prepared/hospital_dirty.csv", "./prepared/hospital_clean.csv"
+        )
--- experiment/errorDetection.py
+++ experiment/errorDetection.py
@@ -3,8 +3,10 @@ import pandas as pd
 import numpy as np
 from ipywidgets import IntProgress
 from IPython.display import display
+from datetime import datetime
 from .setupExperiment import SetupExperiment
 from DataSet import DataSet, serialize_row
+
 
 class ErrorDetection(SetupExperiment):
     def __init__(
@@ -18,20 +20,20 @@ class ErrorDetection(SetupExperiment):
         self.max_row_count = n_rows
         self.dataset = dataset
         # self.logger = Logger(logging_path)
-        
+
     def zero_shot(
-        self, dirty_data = None, 
-        prompt_template: str = "Is there an error in {attr}?\n{context}?", 
-        random_samples = 100, 
-        debug_messages: bool = False
+        self,
+        dirty_data: pd.DataFrame | None = None,
+        prompt_template: str = "Is there an error in {attr}?\n{context}?",
+        random_samples: int = 100,  # TODO: why is self.max_row_count not used here?
+        debug_messages: bool = False,
     ) -> List[int]:
         if dirty_data is None:
             dirty_data, _ = self.dataset.random_sample(random_samples)
-        
+
         progress_bar = IntProgress(
             min=0,
-            max=min(dirty_data.shape[0], self.max_row_count)
-            * dirty_data.shape[1],
+            max=min(dirty_data.shape[0], self.max_row_count) * dirty_data.shape[1],
             description="Attributes Prompted",
         )
         display(progress_bar)
@@ -46,13 +48,13 @@ class ErrorDetection(SetupExperiment):
                     attr=attribute, val=value, context=serialized_row
                 )
                 response = self._prompt(prompt)
-                
+
                 if debug_messages:
                     print(prompt)
                     print("--------------------")
                     print(response)
                     print("====================")
-                
+
                 # evaluate response
                 if "Yes" in response or "yes" in response:
                     classifications.append(1)
@@ -64,21 +66,19 @@ class ErrorDetection(SetupExperiment):
         return classifications
 
     def few_shot(
-        self, 
-        dirty_data = None, 
+        self,
+        dirty_data: pd.DataFrame | None = None,
         promt_template: str = "Is there an error in {attr}?\n\n{example}\n\n{context}?",
-        random_samples:int  = 100, 
+        random_samples: int = 100,  # TODO: why is self.max_row_count not used here?
         example_count: int = 2,
-        debug_messages: bool = False
+        debug_messages: bool = False,
     ) -> List[int]:
-        
         if dirty_data is None:
             dirty_data, _ = self.dataset.random_sample(random_samples)
 
         progressBar = IntProgress(
             min=0,
-            max=min(dirty_data.shape[0], self.max_row_count)
-            * dirty_data.shape[1],
+            max=min(dirty_data.shape[0], self.max_row_count) * dirty_data.shape[1],
             description="Attributes Prompted",
         )
         display(progressBar)
@@ -88,7 +88,9 @@ class ErrorDetection(SetupExperiment):
             serialized_row = serialize_row(row)
             for column, (attribute, _) in enumerate(row.items()):
                 # get examples
-                examples = self.dataset.generate_examples(column_id=column, amount=example_count)
+                examples = self.dataset.generate_examples(
+                    column_id=column, amount=example_count
+                )
 
                 # create prompt
                 prompt = promt_template.format(
