263fcedac03ee79ae78b53127bf30a850ee5d77e
Techno3733 <lucas.kiesel@stud.uni-hannover.de>
2023-12-31 16:12:01 +0100

    Small Logger Crash fix

--- experiment/experimentLogger.py
+++ experiment/experimentLogger.py
@@ -78,8 +78,8 @@ class Logger:
     ) -> None:
         prompt = prompt.replace("\n", "\\n")
         accuracy = (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg)
-        precision = true_pos / (true_pos + false_pos)
-        recall = true_pos / (true_pos + false_neg)
+        precision = true_pos / max(true_pos + false_pos, 1)
+        recall = true_pos / max(true_pos + false_neg, 1)
         with open(f"{self.path}/experiment-results.csv", "a") as file:
             file.write(
                 f'"{namespace}","{name}",{dataset},{n_rows},{n_examples},{runtime},{f1},{precision},{recall},{accuracy},{true_pos},{true_neg},{false_pos},{false_neg},"{prompt}"\n'
